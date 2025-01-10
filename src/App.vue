54c3c43023da238b6529b6cfd0b553b715a463f4
Musti <safi.najjar1@gmail.com>
2025-01-10 08:25:20 +0100

    add footer and header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
 </script>
 
 <template>
-  <RouterView />
+  <AppHeader />
+  <main>
+    <div class="container mx-auto px-3">
+      <RouterView />
+    </div>
+  </main>
+  <AppFooter />
 </template>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="footer footer-center bg-base-300 text-base-content p-4">
+    <aside>
+      <p>Copyright © {{ new Date().getFullYear() }} - All right reserved by ShopApp</p>
+    </aside>
+  </footer>
+</template>
