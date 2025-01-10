22bbda268f96e9b0158693562f1283e671b06818
Musti <safi.najjar1@gmail.com>
2025-01-10 07:43:58 +0100

    add daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+import daisyui from 'daisyui'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ['light', 'dark'],
+  },
 }
